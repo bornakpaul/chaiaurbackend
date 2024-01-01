43627903d4eb16f3e6484763db7f912f16d2afa4
Bornak Paul <bornak.p@gormalone.com>
2024-01-02 00:49:15 +0530

    utils, apiResponse, apiError handler

--- src/index.js
+++ src/index.js
@@ -12,8 +12,9 @@ connectDB().then(()=>{
           console.log("ERROR: ", error);
           throw error;
      });
-     app.listen(process.env.PORT || 8000, ()=> {
-          console.log(`Server is running at port: ${process.env.PORT}`);
+     const port = process.env.PORT || 8000;
+     app.listen(port, ()=> {
+          console.log(`Server is running at port: ${port}`);
      })
 }).catch((error) =>{
      console.log("MONGO db connection failed!!!", error);
